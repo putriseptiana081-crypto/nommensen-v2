b9acac72544982f27d9b98e05812f4ab52fbc3f4
putriseptiana459 <putriseptiana0459@gmail.com>
2026-06-26 17:51:32 +0700

    feat: setup tailwind v4, frontend layout, controller and routing for 6 pages

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Models\History;
-use App\Models\Visimisi;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $history = History::first();
-    $visimisi = Visimisi::first();
-    return view('home', compact('history', 'visimisi'));
+    return view('welcome');
 });
